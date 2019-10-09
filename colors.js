518d1ef4217e7ef76865b0b4201562acfbe5659e
Boris Adamec <boris@giscloud.com>
2019-10-09 14:24:06 +0200

    Add change body backgroundColor

--- colors.js
+++ colors.js
@@ -1,33 +1,40 @@
 const colorNames = [
-	'BlanchedAlmond',
-	'DarkGoldenRod',
-	'ForestGreen',
-	'PeachPuff',
-	'RebeccaPurple',
-	'Fuchsia',
-	'PapayaWhip',
-	'Whitesmoke',
-	'SaddleBrown',
-	'OliveDrab ',
-	'tomato'
+    "BlanchedAlmond",
+    "DarkGoldenRod",
+    "ForestGreen",
+    "PeachPuff",
+    "RebeccaPurple",
+    "Fuchsia",
+    "PapayaWhip",
+    "Whitesmoke",
+    "SaddleBrown",
+    "OliveDrab ",
+    "tomato",
 ];
 
-const radioDiv = document.getElementById('radios');
+const radioDiv = document.getElementById("radios");
+const body = document.getElementsByTagName("body")[0];
 
-const inputs = colorNames.map((color) => {
-	var wrapper = document.createElement('p');
-	var el = document.createElement('input');
+const inputs = colorNames.map(color => {
+    var wrapper = document.createElement("p");
+    var el = document.createElement("input");
 
-	el.className = 'colorInputs';
+    el.className = "colorInputs";
 
-	el.setAttribute('type', 'radio');
-	el.setAttribute('name', 'color');
-	el.setAttribute('value', color);
+    el.setAttribute("type", "radio");
+    el.setAttribute("name", "color");
+    el.setAttribute("value", color);
 
-	wrapper.append(el);
-	wrapper.insertAdjacentHTML('beforeend', ` ${color}<br>`);
+    el.addEventListener("change", function(e) {
+        console.log("e", e);
 
-	return wrapper;
+        body.style.backgroundColor = e.target.value;
+    });
+
+    wrapper.append(el);
+    wrapper.insertAdjacentHTML("beforeend", ` ${color}<br>`);
+
+    return wrapper;
 });
 
 radioDiv.append(...inputs);
