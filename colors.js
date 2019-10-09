3eab98d2ef56af486ed139f4627a7893988159a3
Boris Adamec <boris@giscloud.com>
2019-10-09 10:40:42 +0200

    Add html and radio btns

--- colors.js
+++ colors.js
@@ -1,7 +1,26 @@
-const names = [
+const colorNames = [
     "BlanchedAlmond",
     "DarkGoldenRod",
     "ForestGreen",
     "PeachPuff",
     "RebeccaPurple",
 ];
+
+const radioDiv = document.getElementById("radios");
+
+const inputs = colorNames.map(color => {
+    var wrapper = document.createElement("p");
+    var el = document.createElement("input");
+
+    el.className = "colorInputs";
+
+    el.setAttribute("type", "radio");
+    el.setAttribute("name", "color");
+    el.setAttribute("value", color);
+
+    wrapper.append(el);
+
+    return wrapper;
+});
+
+radioDiv.append(...inputs);
