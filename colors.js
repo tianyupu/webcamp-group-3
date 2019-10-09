40570572d93bbc2d16b3fd8d028ce9e822d38fce
Mateja Cimpersak <matejacimpersak@gmail.com>
2019-10-09 12:49:50 +0200

    added tomato color

--- colors.js
+++ colors.js
@@ -1,32 +1,33 @@
 const colorNames = [
-    "BlanchedAlmond",
-    "DarkGoldenRod",
-    "ForestGreen",
-    "PeachPuff",
-    "RebeccaPurple",
-    "Fuchsia",
-    "PapayaWhip",
-    "Whitesmoke",
-    "SaddleBrown",
-    "OliveDrab ",
+	'BlanchedAlmond',
+	'DarkGoldenRod',
+	'ForestGreen',
+	'PeachPuff',
+	'RebeccaPurple',
+	'Fuchsia',
+	'PapayaWhip',
+	'Whitesmoke',
+	'SaddleBrown',
+	'OliveDrab ',
+	'tomato'
 ];
 
-const radioDiv = document.getElementById("radios");
+const radioDiv = document.getElementById('radios');
 
-const inputs = colorNames.map(color => {
-    var wrapper = document.createElement("p");
-    var el = document.createElement("input");
+const inputs = colorNames.map((color) => {
+	var wrapper = document.createElement('p');
+	var el = document.createElement('input');
 
-    el.className = "colorInputs";
+	el.className = 'colorInputs';
 
-    el.setAttribute("type", "radio");
-    el.setAttribute("name", "color");
-    el.setAttribute("value", color);
+	el.setAttribute('type', 'radio');
+	el.setAttribute('name', 'color');
+	el.setAttribute('value', color);
 
-    wrapper.append(el);
-    wrapper.insertAdjacentHTML("beforeend", ` ${color}<br>`);
+	wrapper.append(el);
+	wrapper.insertAdjacentHTML('beforeend', ` ${color}<br>`);
 
-    return wrapper;
+	return wrapper;
 });
 
 radioDiv.append(...inputs);
